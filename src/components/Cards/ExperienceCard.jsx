1ba0b9b627b74fbb38057c2b5c7b5e2c56d2fb5e
Christian Garcerant <christian.garcerant@uac.edu.co>
2023-12-28 13:43:00 -0500

    STYLES Experience card upgrade

--- src/components/Cards/ExperienceCard.jsx
+++ src/components/Cards/ExperienceCard.jsx
@@ -36,9 +36,9 @@ text-overflow: ellipsis;
 `
 
 const Card = styled.div`
-    width: 650px;
+  background: ${({ theme }) => theme.card};
+    width: 800px;
     border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
     padding: 12px 16px;
     justify-content: space-between;
     position: relative;
@@ -54,7 +54,7 @@ const Card = styled.div`
     @media only screen and (max-width: 768px){
         padding: 10px;
         gap: 8px;
-        width: 300px;
+        width: 320px;
     }
 
     &:hover ${Document}{
@@ -67,8 +67,8 @@ const Card = styled.div`
 
     }
 
-    border: 0.1px solid #306EE8;
-    box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+    border: 0.1px  solid rgba(0, 0, 0, 0.2);
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 24px;
 `
 
 const Top = styled.div`
